9f5c64c3c91fa971e117bdfaa02f32b544ee2952
tubabalkan <100600148+tubabalkan@users.noreply.github.com>
2025-04-26 20:09:40 +0300

    ....

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminFooterAdressController.cs
@@ -0,0 +1,95 @@
+﻿using CarBook.Dto.AuthorDtos;
+using CarBook.Dto.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminFooterAdress")]
+    public class AdminFooterAdressController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFooterAdressController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7001/api/FooterAddreses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [Route("CreateFooterAdress")]
+        [HttpGet]
+        public IActionResult CreateFooterAdress()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateFooterAdress")]
+        public async Task<IActionResult> CreateFooterAdress(CreateAuthorDto createAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAuthorDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7001/api/FooterAddreses", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAdress", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("RemoveFooterAdress/{id}")]
+        public async Task<IActionResult> RemoveFooterAdress(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7001/api/FooterAddreses?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAdress", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("UpdateFooterAdress/{id}")]
+        public async Task<IActionResult> UpdateFooterAdress(int id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7001/api/FooterAddreses/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFooterAdressDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("UpdateFooterAdress/{id}")]
+        public async Task<IActionResult> UpdateFooterAdress(UpdateFooterAdressDto updateFooterAdressDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateFooterAdressDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"https://localhost:7001/api/FooterAddreses", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
